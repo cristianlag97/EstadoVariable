47f4bf32c2b559bcee3a12929e905b64bb42871b
Cristian_Laguna <cdlaguna0@misena.edu.co>
2019-10-17 16:40:35 -0500

    Agregando nuevo Proyecto

---
+++ EstadoVariable/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EstadoVariable
+{
+    public partial class Form1 : Form
+    {
+        int numero = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TxtNumero_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void BtnContar_Click(object sender, EventArgs e)
+        {
+            // int numero = 0;
+            //for(int i=0; i<100; i++)
+            // {
+            //     numero = numero + 1;
+            //     txtNumero.Text = numero.ToString();
+            // }
+          
+            numero = numero + 1;
+            txtNumero.Text = numero.ToString();
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtNumero.Text = "0";
+        }
+    }
+}
---
+++ Requerimiento/primero.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="primero.aspx.cs" Inherits="Requerimiento.primero" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:GridView ID="grilla" runat="server" AutoGenerateColums="false">
+          <columns>
+              <asp:BoundField DataField="nombre" HeaderText="Nombre" SortExpression="nombre" />
+              <asp:BoundField DataField="apellido" HeaderText="Apellido" SortExpression="apellido" />
+              <asp:BoundField DataField="cedula" HeaderText="Cedula" SortExpression="cedula" />
+        </columns>
+        </asp:GridView>
+    </form>
+</body>
+</html>
